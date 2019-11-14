6e48a93dbd43d3a95967d20f865eda384057d410
serena Lee <dearserenalee@gmail.com>
2019-11-14 09:20:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,10 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids)
-  if birthday_kids <= 12
-    birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+def age_appropriate_birthday(birthday_kid)
+  if birthday_kid <= 12
+    birthday_kid.each do |kid_name, age|
+    puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
   end 
 end 
 
